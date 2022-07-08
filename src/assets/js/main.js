3a641f10009657ceba05ea2a1a33b7c85bed6780
ionos-deploy-now-support <markus.hunsalz@ionos.com>
2022-07-08 15:48:56 +0200

    fixed inline scripting

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,19 @@
-document.getElementById("go-back").addEventListener("click", historyBack);
+document
+  .getElementById("scanA")
+  .addEventListener("click", (evt) =>
+    scan("%2Fsecurity%2Fheaders%2F&followRedirects=on", evt)
+  );
 
-function historyBack() {
-    history.back(-1);
-};
+document
+  .getElementById("scanF")
+  .addEventListener("click", (evt) =>
+    scan("%2F&followRedirects=on", evt)
+  );
+
+function scan(path, evt) {
+  let hostname = location.hostname;
+  if (hostname === "localhost") {
+    hostname = "https://htaccess-samples.space";
+  }
+  window.open("https://securityheaders.com/?q=" + hostname + path, "_blank");
+}
